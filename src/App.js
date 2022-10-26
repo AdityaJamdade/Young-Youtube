c6529921911122f8495d72b0ad26b906c7542892
AdityaJamdade <adijamdade156@gmail.com>
2022-10-26 10:06:28 +0530

    completed the top navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { Navbar, SearchFeed, VideoDetail, ChannelDetail, Feed } from './components';
 
 const App = () => (
     <BrowserRouter>
         <Box sx={{backgroundColor: '#000'}}>
-            Navbar
+            <Navbar />
             <Routes>
                 <Route path='/' exact element={<Feed />}/>
                 <Route path='/video/:id' element={<VideoDetail />} />
