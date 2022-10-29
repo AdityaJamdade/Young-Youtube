dba4547eb166460199cf5dc192dae81490d9ca7d
AdityaJamdade <adijamdade156@gmail.com>
2022-10-29 12:58:06 +0530

    fixed minor sidebar issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,8 +19,8 @@ const Sidebar = () => (
                 style={{background: category.name === selectedCategory && '#FC1503', color:'white'}}
                 key={category.name}
             >
-                <span style={{ color: cateory.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.name}</span>
-                <span>{category.icon}</span>
+                <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+                <span style={{opacity:category.name===selectedCategory ? '1' : '0.8'}}>{category.name}</span>
             </button>
         ))}
     </Stack>
