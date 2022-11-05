ac616183566100df7cf4957be348e3d17e7f64e2
AdityaJamdade <adijamdade156@gmail.com>
2022-11-05 13:29:14 +0530

    fetching of data from rapid api is complete

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material'
 
-const Videos = () => {
+const Videos = ({ videos }) => {
+
+  console.log(videos);
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {/* {item.id.videoId && <VideoCard video={item} />} */}
+          {/* {item.id.channelId && <ChannelCard video={item} />} */}
+          <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
